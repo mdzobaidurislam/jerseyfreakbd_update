bc7a6b0b077aab76b144cfdb3865cc97504233b3
mdzobaidurislam <freelancerjami@gmail.com>
2024-11-25 22:21:39 +0600

    api chnage

--- app/config/api.tsx
+++ app/config/api.tsx
@@ -4,9 +4,9 @@
 // export const REVALIDATE = 10;
 
 
-// export const API_BASE_URL = 'https://jerseyfreak.techdynobdltd.com/api/v2';
-// export const BASE_URL = 'https://jerseyfreak.techdynobdltd.com';
-// export const REVALIDATE = 10;
-export const API_BASE_URL = 'https://admin.jerseyfreakbd.com/api/v2';
-export const BASE_URL = 'https://admin.jerseyfreakbd.com';
+export const API_BASE_URL = 'https://jerseyfreak.techdynobdltd.com/api/v2';
+export const BASE_URL = 'https://jerseyfreak.techdynobdltd.com';
 export const REVALIDATE = 10;
+// export const API_BASE_URL = 'https://admin.jerseyfreakbd.com/api/v2';
+// export const BASE_URL = 'https://admin.jerseyfreakbd.com';
+// export const REVALIDATE = 10;
